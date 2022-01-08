ff0e8b83e9c6f589b1519f0c4e3b569ea1f3a76d
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2022-01-08 21:39:45 +0300

    JSON request via AlertController + formatting of search for cities with names, consisting of several words

--- Sunny/View Controllers/ViewController.swift
+++ Sunny/View Controllers/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     let networkManager = NetworkManager()
     
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
+            self.networkManager.fetchCurrentWeather(forCity: city)
+        }
     }
     
     override func viewDidLoad() {
